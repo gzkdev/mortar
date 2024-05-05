1978ec571eb61a7eb3755900487e75a13d05fa52
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-05 22:14:34 +0100

    add web3 provider config

--- src/providers/web3-provider.tsx
+++ src/providers/web3-provider.tsx
@@ -1,3 +1,29 @@
-export function Web3Provider() {
-  return <div>web3-providers</div>;
-}
+"use client";
+
+import { mainnet } from "wagmi/chains";
+import { WagmiProvider, createConfig } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+
+const config = createConfig(
+  getDefaultConfig({
+    chains: [mainnet],
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+    appName: "Mortar",
+    appDescription: "A light weight blueprint for building decentralized apps",
+    appUrl: "https://family.co",
+    appIcon: "https://family.co/logo.png",
+  })
+);
+
+const queryClient = new QueryClient();
+
+export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
