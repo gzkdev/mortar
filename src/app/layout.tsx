266de0a3056777aee5b305c269416c4c0e5707c8
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-05 22:17:33 +0100

    add provider to app root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Web3Provider } from "@/providers/web3-provider";
 
 export const metadata: Metadata = {
   title: "Mortar",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <Web3Provider>
+        <body>{children}</body>
+      </Web3Provider>
     </html>
   );
 }
