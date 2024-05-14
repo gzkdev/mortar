253e8ce0582206a7694dba00947f4b7f85fd70fb
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-14 23:03:11 +0100

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,14 @@ export default function Home() {
   const { setOpen } = useModal();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-6 text-center space-y-10 text-xl">
-      <h1 className="text-6xl font-semibold">Mortar</h1>
-      <p>A light weight blueprint for building decentralized apps</p>
+    <main className="flex min-h-screen flex-col items-center justify-center p-6 text-center gap-4">
+      <h1 className="text-4xl font-bold">Mortar</h1>
+      <p className="mb-4">
+        A light weight blueprint for building decentralized apps
+      </p>
       <button
         onClick={() => setOpen(true)}
-        className="py-4 px-6 rounded-full inline-flex outline bg-zinc-800 text-zinc-50 hover:bg-zinc-700 transition"
+        className="py-2 px-4 rounded-lg inline-flex outline bg-zinc-800 text-zinc-50 hover:bg-zinc-700 transition"
       >
         Connect wallet
       </button>
