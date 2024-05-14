9d7b3cec3d6a62991b7e3965c6720140385f2daa
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-14 23:04:17 +0100

    add inter font to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import { Inter } from "next/font/google";
 import { Web3Provider } from "@/providers/web3-provider";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mortar",
@@ -15,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Web3Provider>
-        <body>{children}</body>
+        <body className={inter.className}>{children}</body>
       </Web3Provider>
     </html>
   );
